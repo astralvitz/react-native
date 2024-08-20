6ae1e74a222894fa2e872357642d82587fd06aa3
xlcrr <info@openlittermap.com>
2024-08-20 22:44:58 +0100

    more updates

--- android/app/src/main/java/com/openlittermap/MainActivity.kt
+++ android/app/src/main/java/com/openlittermap/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.openlittermap
+package com.geotech.openlittermap
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/openlittermap/MainApplication.kt
+++ android/app/src/main/java/com/openlittermap/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.openlittermap
+package com.geotech.openlittermap
 
 import android.app.Application
 import com.facebook.react.PackageList
